363aa7b552ffe4dc3f05e88c695406033edf66d8
Wan Ardie Mejia <wan_ahmad_ardie@yahoo.com>
2020-01-25 18:03:07 +0800

    only 1 include file test

---
+++ includes/test1.php
@@ -0,0 +1,53 @@
+<?php
+
+function article_title_1(){
+?>
+    Failed Tenon Saw
+<?php
+}
+
+function article_content_1(){
+?>
+    This is my failed tenon saw. It was my first ever attempt at making a tenon saw with a brass back. The real devil that defeated this project was the brass back. Steel bending is not a joke, it is a skill my itself. At the time of making this, I dremeled a rough groove with no jig to guide me, and bent it following a Youtube guide online, where the guy hammered a steel held in a vise to bend it. Ther result was quite ugly, and not straight. Bending it was also not easy, despite dremeling to almost 1/8 " thickness. If I would redo this, I would spend time making a proper dremel guiding jig, heating the brass to a high temperature, to make it softer.
+<?php
+}
+
+function article_image_1(){
+    return "failed-tenon-saw.jpg";
+}
+
+function article_title_2(){
+?>
+    My 2 planes that I own
+<?php
+}
+
+function article_content_2(){
+?>
+    These are the 2 planes that I currently own and use consistently. Both bought at an extreme bargain costing RM 20 each (4.90 in US dollar). This was an interesting experiment in restoration. Both had been 'restored' after I bought them, but only worked half the time. The red painted plane, after some research, turned out to be a rare Stanley plane, H-104, that turned out to be a failed commercial concept at the time. As i scoured sources online, it had a red lever cap missing. I thought the screws tightening the blade and cap-iron piece to the frog would be enough, but no matter how much I tightened the screw, the blade still had some vibration. It was only after giving up on the red plane after 1 year, I cam back to it with improved steel skills. My new solution was to heat the cap iron to cherry-red (forging temperature) and bend on a vise. This resulted in super-tight fit of the blade with the cap-iron, so no need to rely on the frog to tighten these 2. This was a fix I came up after some amateur attempt at steel skills. This plane also is a collectible due to its rarity, so am I losing something here? The other wooden plane, also deserves a story, which is in the next article ->
+<?php
+}
+
+function article_image_2(){
+    return "2-planes.jpg";
+}
+
+
+function article_title_3(){
+?>
+    My first wooden plane
+<?php
+}
+
+function article_content_3(){
+?>
+    This plane was bought at only RM 20. I had been 'fixing' this plane every 2 months or so. From slight changes to the sharpening angle, to gradual removal of the mouth  material, which I thought (correctly at first) was the primary reason for trapped shavings. The mouth is now a bit huge. After laying off (always a smart thing to do, if youre not sure your improving your tools) for almost 1 year, I decided it was the bedding angle of the plane. I discovered it was 60 degrees, compared to my other 55 degrees plane. Considering that planes are not my primary skill, I slowly chiseled away, then filed, then chiseled and files all over again, until I was sure the bedding surface was completely flat. Then I learned that wedges should have a sharp angle somewhere around 15 degrees, and I learnt a trick to refine the wedge to perfection between the new steel rod and the blade. 
+<?php
+}
+
+function article_image_3(){
+    return "fixing-old-plane.jpg";
+}
+
+?>
+
