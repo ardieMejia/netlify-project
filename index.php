771276e0573c12cdd57bf8af6eb290f933411635
Wan Ardie Mejia <wan_ahmad_ardie@yahoo.com>
2020-08-03 12:59:09 +0800

    make it better 2

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My app</title>
+        <script
+			      src="https://code.jquery.com/jquery-3.4.1.js"
+			      integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+			      crossorigin="anonymous"></script>
+        <script type="text/javascript" src="./javascript/my-first.js"></script>
+        <link rel="stylesheet" type="text/css" href="./css/my-styles.css">
+        
+		    
+
+
+
+    </head>
+    <body>
+        
+
+
+
+
+
+
+        <nav>
+            <ul>
+                <li class="sub-menu-parent" tab-index="0">
+                    <a href="http://google.com">Home</a>
+                    <div class="space"></div>
+                    <ul class="sub-menu">
+                        <!--  
+                        <li class="space"><a></a></li> empty space hack 
+                        <li><a href="#">Sub Item 1</a></li>
+                        <li><a href="#">Sub Item 2</a></li>
+                        <li><a href="#">Sub Item 3</a></li>
+                        <li><a href="#">Sub Item 4</a></li>
+                        -->
+                    </ul>
+                </li>
+                <li class="sub-menu-parent" tab-index="0">
+                    <a href="#">Image Gallery</a>
+                    <ul class="sub-menu">
+                        <li class="space"><a></a></li> <!-- empty space hack -->
+                        <li><a href="#">Search</a></li>
+                        <li><a href="#">My handmade tools</a></li>
+                        <li><a href="#">Other woodworkers</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                    </ul>
+                </li>
+                <li class="sub-menu-parent" tab-index="0">
+                    <a href="#">Information</a>
+                    <div class="space"></div>
+                    <ul class="sub-menu">
+                        <li class="space"><a></a></li> <!-- empty space hack -->
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul></li>
+            </ul>
+        </nav>
+
+
+        <h1>My Woodworking Projects 2020</h1>
+<div class="row">
+<div class="column">
+        <div class="card">
+            
+            <div class="container">
+                <h4><b>Failed Tenon Saw</b></h4>
+
+
+                <p>
+                    This is my failed tenon saw. It was my first ever attempt at making a tenon saw with a brass back. The real devil that defeated this project was the brass back. Steel bending is not a joke, it is a skill my itself. At the time of making this, I dremeled a rough groove with no jig to guide me, and bent it following a Youtube guide online, where the guy hammered a steel held in a vise to bend it. Ther result was quite ugly, and not straight. Bending it was also not easy, despite dremeling to almost 1/8 " thickness. If I would redo this, I would spend time making a proper dremel guiding jig, heating the brass to a high temperature, to make it softer.
+                </p>
+
+
+            </div>
+
+            <div class="img-container">
+                    <img src="./images/failed-tenon-saw.jpg" alt="tenon saw" style="width:50%">
+            </div>
+
+        </div>
+</div>
+
+<div class="column">
+        <div class="card">
+            
+            <div class="container">
+                <h4><b>My 2 planes that I own</b></h4> 
+                    
+                <p>
+                    These are the 2 planes that I currently own and use consistently. Both bought at an extreme bargain costing RM 20 each (4.90 in US dollar). This was an interesting experiment in restoration. Both had been 'restored' after I bought them, but only worked half the time. The red painted plane, after some research, turned out to be a rare Stanley plane, H-104, that turned out to be a failed commercial concept at the time. As i scoured sources online, it had a red lever cap missing. I thought the screws tightening the blade and cap-iron piece to the frog would be enough, but no matter how much I tightened the screw, the blade still had some vibration. It was only after giving up on the red plane after 1 year, I cam back to it with improved steel skills. My new solution was to heat the cap iron to cherry-red (forging temperature) and bend on a vise. This resulted in super-tight fit of the blade with the cap-iron, so no need to rely on the frog to tighten these 2. This was a fix I came up after some amateur attempt at steel skills. This plane also is a collectible due to its rarity, so am I losing something here? The other wooden plane, also deserves a story, in an upcoming article ->
+                </p> 
+            </div>
+            <div class="img-container">
+                <img src="./images/2-planes.jpg" alt="tenon saw" style="width:50%">
+            </div>
+        </div>
+</div>
+
+
+
+
+
+
+<div class="column">
+    <div class="card">
+        
+        <div class="container">
+            <h4><b>My first wooden plane</b></h4> 
+            <p>
+                This plane was bought at only RM 20. I had been 'fixing' this plane every 2 months or so. From slight changes to the sharpening angle, to gradual removal of the mouth  material, which I thought (correctly at first) was the primary reason for trapped shavings. The mouth is now a bit huge. After laying off (always a smart thing to do, if youre not sure your improving your tools) for almost 1 year, I decided it was the bedding angle of the plane. I discovered it was 60 degrees, compared to my other 55 degrees plane. Considering that planes are not my primary skill, I slowly chiseled away, then filed, then chiseled and files all over again, until I was sure the bedding surface was completely flat. Then I learned that wedges should have a sharp angle somewhere around 15 degrees, and I learnt a trick to refine the wedge to perfection between the new steel rod and the blade. 
+            </p> 
+        </div>
+        <div class="img-container">
+            <img src="./images/fixing-old-plane.jpg" alt="tenon saw" style="width:50%">
+        </div>
+    </div>
+</div>
+
+
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    
+                    <div class="container">
+                        <h4><b>My First Moxon Vise</b></h4> 
+                        <p>
+                            This is my first moxon vise. This project turns out better than I expected, and pretty good for a first moxon vise build. Now that Im using constantly, it needs to be lined with leather as the wood flexes a bit much. Im guessing the wooden jaw doesnt have enough mass. The jaw is also too low that my plane would almost hit the screw handle. The leather will probably make it last longer as well, though im guessing this vise as of now, will certainly last more than 1 year of constant use. Overall, Im happy with this one, and certainly an improvement over my current work area.
+                        </p> 
+                    </div>
+                    <div class="img-container">
+                        <img src="./images/my-first-moxon-vise.jpg" alt="tenon saw" style="width:50%">
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+</div>
+
+    </body> 
+</html>
+
+</html>
+
+
+
+
+
