ba83bc1c47daeff4f5264c5e756138210343290d
Wan Ardie Mejia <wan_ahmad_ardie@yahoo.com>
2020-08-09 09:43:44 +0800

    my-first.js now consistent with netlify repo

--- javascript/my-first.js
+++ javascript/my-first.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
 
+
+
+
+
     var navLeft = $('.cont-navLeft');
     var showLeft = $('#left-menu');
-    
-   
-    
+
+
+
     $(showLeft).click(function(){
 		    $(navLeft).toggleClass("openLeft");
 		    $(document.body).toggleClass("push-right");
 		    return false;
-	  });
-    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
+
