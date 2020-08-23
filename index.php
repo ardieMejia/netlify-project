395a0524927505c9adde9b642ff0425a4f5c51b4
Wan Ardie Mejia <wan_ahmad_ardie@yahoo.com>
2020-08-23 21:13:14 +0800

    some changes index.php

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My app</title>
+        <script
+			      src="https://code.jquery.com/jquery-3.4.1.js"
+			      integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+			      crossorigin="anonymous"></script>
+        <script type="text/javascript" src="/javascript/FeedEk.js"></script>
+        <script type="text/javascript" src="/javascript/my-first.js"></script>
+        <link rel="stylesheet" type="text/css" href="./css/my-styles.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+        
+		    
+
+
+
+    </head>
+    <body>
+
+
+
+
+
+
+
+        <nav>
+            <ul>
+                <li class="sub-menu-parent-left" tab-index="0">
+                    <a href="/sm/sm_RSS_feeds.html"><img src="/images/moreRSS_feed/chaos_RSS-logo.png"/></a>
+                    <div class="space"></div>
+
+                </li>
+                <li class="sub-menu-parent" tab-index="0">
+                    <a href="#">Home</a>
+                    <div class="space"></div>
+                    <ul class="sub-menu">
+                        <!--
+                             <li class="space"><a></a></li> empty space hack
+                             <li><a href="#">Sub Item 1</a></li>
+                             <li><a href="#">Sub Item 2</a></li>
+                             <li><a href="#">Sub Item 3</a></li>
+                             <li><a href="#">Sub Item 4</a></li>
+                        -->
+                    </ul>
+                </li>
+                <li class="sub-menu-parent" tab-index="0">
+                    <a href="#">Image Gallery</a>
+                    <ul class="sub-menu">
+                        <li class="space"><a></a></li> <!-- empty space hack -->
+                        <li><a href="#">Search</a></li>
+                        <li><a href="./sm/sm_image_gallery.html">My handmade tools</a></li>
+                        <li><a href="/sm/sm_forgot_commands.html">Command lines I keep forgetting</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                    </ul>
+                </li>
+                <li class="sub-menu-parent" tab-index="0">
+                    <a href="#">Information</a>
+                    <div class="space"></div>
+                    <ul class="sub-menu">
+                        <li class="space"><a></a></li> <!-- empty space hack -->
+                        <li><a href="/sm/sm_pdfs_books.html">Books</a></li>
+                        <li><a href="/sm/sm_links.html">Links</a></li>
+                        <li><a href="/sm/sm_archive.html">Archive</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul></li>
+
+            </ul>
+        </nav>
+
+
+        <h1>My Wishlist</h1>
+<div class="row">
+    <div class="column">
+        <div class="card deep-purple lighten-5">
+
+            <div class="img-container-product">
+                <a target="_blank" href="https://displate.com/sr-artworks/woodworking">
+                    <img src="./images/displate_logo.png" alt="Displate Logo">
+                </a>
+
+            </div>
+
+            <div class="container">
+
+                <h2 class="product-fanciness" >
+                    Measure Twice Cut Once Then Force it to Fit
+
+                </h2>
+                <h3 class="product-fanciness">Handcrafted metal posters<br/><br/></h3>
+
+
+
+
+            </div>
+
+
+
+        </div>
+</div>
+
+<div class="column">
+    <div class="card">
+
+
+        <div class="img-container-product">
+            <a target="_blank" href="http://www.veritastools.com/Home.aspx">
+                <img src="./images/VeritasLogo-EN.png" alt="Veritas logo">
+            </a>
+
+        </div>
+            <div class="container">
+
+                
+                <h2 class="product-fanciness" >
+                    Veritas means truth
+                </h2>
+
+
+            </div>
+
+        </div>
+</div>
+<div class="column">
+    <div class="card">
+
+
+        <div class="img-container-product">
+            <a target="_blank" href="https://www.etsy.com/in-en/listing/89985396/norwegian-lap-clamp-for-classic-boat?show_sold_out_detail=1">
+                <img src="./images/Etsy-emblem-med.png" alt="Etsy Logo">
+            </a>
+        </div>
+        <div class="container">
+
+            
+            <h2 class="product-fanciness" >
+                Lap Clamp (used in boatmaking) on Etsy
+            </h2>
+
+        </div>
+
+    </div>
+</div>
+
+<div class="column">
+    <div class="card">
+
+
+        <div class="img-container-product">
+            <a target="_blank" href="https://www.fine-tools.com/liogier.html">
+                <img src="./images/logo-liogier_ORI.png" alt="Liogier Logo">
+            </a>
+        </div>
+        <div class="container">
+
+            
+            <h2 class="product-fanciness" >
+                <i>Liogier</i> hand-stitched rasps
+            </h2>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+<script type="text/javascript" src="js/materialize.min.js"></script>
+    </body> 
+</html>
+
+</html>
+
+
+
+
+
